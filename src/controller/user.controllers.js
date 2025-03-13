25aa4b446b5320b2fe2001293495d52bc6db230f
M-A-SAIADITHYAA <145114909+M-A-SAIADITHYAA@users.noreply.github.com>
2025-03-13 23:08:32 +0530

    add login functionality with access and refresh token generation

--- src/controller/user.controllers.js
+++ src/controller/user.controllers.js
@@ -111,6 +111,71 @@ const registered_user =  asyncHandler(async(req,res)=>{
     }
 })
 
+const generateAccessandRefreshToken = async (userId) =>{
+    try {
+        const user = await User.findById(userId)
+    
+        if(!user){
+            throw new ApiError(400,"User not found")
+        }
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+    
+        user.refreshToken = refreshToken
+        return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong with refreshToken ")
+        
+    }
+}
+
+const loginUser = asyncHandler(async (req,res)=>{
+    //get the data from the body
+    const {email,username,password} = req.body
+
+    if(!email){
+        throw new ApiError(500,"email is required")
+    }
+
+    const user = await User.findOne({
+        $or : [{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(500,"User not found")
+    }
+
+    const isPassworValid  = await user.isPasswordCorrect(password)
+    if(!isPassworValid){{
+        throw new ApiError(500,"Invalid credentials")
+    }}
+
+    const {accessToken,refreshToken} = await generateAccessandRefreshToken(user._id)
+    const loggedInUser = await User.findById
+    (user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!loggedInUser){
+        throw new ApiError(500,"user not found in database")
+    }
+
+    const options = {
+        httpOnly : true,
+        secure: process.env.NODE_ENV === "production",
+
+    }
+
+    return res
+    .status(200) 
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(new ApiResponse(200,
+        {user:loggedInUser,refreshToken,accessToken},
+        "User logged in successfully"))
+    
+})
 export {
     registered_user,
+    loginUser
 }
